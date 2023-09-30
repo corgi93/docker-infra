3e510f90ddbbcea192415397c22d2f91c5d14742
hyeokjinJang <hjjang@dawin.xyz>
2023-10-01 02:20:18 +0900

    [web] init project

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+# 하이퍼 크루
+
+우리동네 크루 (육아크루 , 스터디크루, 개발크루)
+
+## stack
+
+- Next 13 (app router)
+- tailwindCSS
---
+++ frontend/src/shared/config/constants.ts
@@ -0,0 +1,20 @@
+import { NavDataTypes } from "./types";
+
+export const navData: NavDataTypes[] = [
+  {
+    name: "홈",
+    path: "/",
+  },
+  {
+    name: "소개",
+    path: "/about",
+  },
+  {
+    name: "로그인",
+    path: "/signin",
+  },
+  {
+    name: "회원가입",
+    path: "/signup",
+  },
+];
